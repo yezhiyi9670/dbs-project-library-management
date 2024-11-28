61be7cb9452390a343acab8507a552727301af3d
yezhiyi9670 <yezhiyi0562@foxmail.com>
2024-11-28 18:11:30 +0800

    Terminal: Do not logout already-logged-out users

--- frontend/src/app-terminal/pages/LogoutPage.vue
+++ frontend/src/app-terminal/pages/LogoutPage.vue
@@ -3,6 +3,7 @@ import ToCenter from '../../component/ToCenter.vue';
 import { Api } from '../../api/Api';
 import { useAppContext } from '../../context/AppContext';
 import { useRouter } from 'vue-router';
+import PermissionGuard from '../component/PermissionGuard.vue';
 
 const appContext = useAppContext()
 const router = useRouter()
@@ -20,13 +21,17 @@ async function handleLogout() {
   }
 }
 
-handleLogout()
+if(appContext.value.isLoggedIn()) {
+  handleLogout()
+}
 
 </script>
 <template>
-  <ToCenter>
-    <v-icon icon="mdi-exit-run" size="80" />
-    <div class="mt-4" style="font-size:1.5em">正在退出登录</div>
-    <div class="mt-2">为了你的账户安全，请确认退出完成再离开</div>
-  </ToCenter>
+  <PermissionGuard>
+    <ToCenter>
+      <v-icon icon="mdi-exit-run" size="80" />
+      <div class="mt-4" style="font-size:1.5em">正在退出登录</div>
+      <div class="mt-2">为了你的账户安全，请确认退出完成再离开</div>
+    </ToCenter>
+  </PermissionGuard>
 </template>
